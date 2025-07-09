77491591fd31a6752d279d50c71a384b517be35d
Matheesha Anusari <anusarimatheesha@gmail.com>
2025-07-09 12:28:30 +0530

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return  view('home');
+})->name('home');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', function () {
+        return Inertia::render('dashboard');
+    })->name('dashboard');
+});
+
+
+Route::get('/donate', function () {
+    return view('donate'); 
+})->name('donate');
+
+
+
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
